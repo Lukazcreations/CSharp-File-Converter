bae589e5fb627b9cfca381aa8a8835980eb3e823
Luqman Bello <hello@luqmanbello.com>
2021-04-20 12:43:42 +0100

    Add project files.

---
+++ FileConverter/MDAList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileConverter
+{
+    public class MDAListItem
+    {
+        public string mdaCode { get; set; }
+        public string mdaName { get; set; }
+    }
+
+    public class MDAList
+    {
+        public List<MDAList> MDAListItems { get; set; }
+    }
+
+}
---
+++ FileConverter/Program.cs
@@ -0,0 +1,30 @@
+﻿using CsvHelper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read file into a string and deserialize JSON to a type
+            var MDAListFile = JsonConvert.DeserializeObject<List<MDAListItem>>(File.ReadAllText(@"C:\Users\lbello2\Desktop\mdaList.txt").Replace("MDAList:",string.Empty));
+
+            // write the file to csv
+            using (var writer = new StreamWriter(@"C:\Users\lbello2\Desktop\convertedfile.csv", true))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(MDAListFile);
+            }
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
